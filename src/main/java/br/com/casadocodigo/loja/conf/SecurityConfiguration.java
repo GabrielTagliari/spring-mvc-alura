80a3f36cb0b8907404e3fc5d7473f530ad2e12ac
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2017-10-29 21:50:30 -0200

    Adicionada permissão resources, configuração da ulr de login e logout

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.com.casadocodigo.loja.dao.UsuarioDao;
 
@@ -22,9 +23,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 			.antMatchers("/produtos").hasRole("ADMIN")
 			.antMatchers("/carrinho/**").permitAll()
 			.antMatchers("/produtos/**").permitAll()
+			.antMatchers("/resources/**").permitAll()
 			.antMatchers("/").permitAll()
 			.anyRequest().authenticated()
-			.and().formLogin();
+			.and().formLogin().loginPage("/login").permitAll()
+			.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 	
 	@Override
